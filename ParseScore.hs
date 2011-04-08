056713f5942d58db83281807ec12eb6a148491f1
Sam Phippen <samphippen@googlemail.com>
2011-04-08 16:58:37 +0100

    fix redundant brackets in ParseScore.hs
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- ParseScore.hs
+++ ParseScore.hs
@@ -24,7 +24,7 @@ _score tokens supers (RampUp : rest) = 3 + _score tokens supers rest
 _score tokens supers (RampDown : rest) = 3 + _score tokens supers rest
 _score tokens supers (Corner : rest) = 2 + (tokens + supers) + _score tokens supers rest
 _score tokens supers (SuperUp : rest) = _score tokens (supers + 1) rest
-_score tokens supers (SuperDown : rest) = _score tokens (supers) rest
+_score tokens supers (SuperDown : rest) = _score tokens supers rest
 _score tokens supers [] =  if (tokens > 0) then 2 else 0
 
 hasSuperAtEnd :: [Token] -> Bool
