7957c7e32347392b9d7a9bd33d3ee08166fe3758
Sam Phippen <samphippen@googlemail.com>
2011-04-08 16:38:36 +0100

    update the parserscore file to work with the rules
    it wasn't counting points for tokens at the end of the game
    
    Signed-off-by: Sam Phippen <samphippen@googlemail.com>

--- ParseScore.hs
+++ ParseScore.hs
@@ -27,7 +27,7 @@ _score tokens supers (RampDown : rest) = 3 + _score tokens supers rest
 _score tokens supers (Corner : rest) = 2 + (tokens + supers) + _score tokens supers rest
 _score tokens supers (SuperUp : rest) = _score tokens (supers + 1) rest
 _score tokens supers (SuperDown : rest) = _score tokens (supers) rest
-_score tokens supers [] = 0
+_score tokens supers [] =  if (tokens > 0) then 2 else 0
 
 hasSuperAtEnd :: [Token] -> Bool
 hasSuperAtEnd a = ((reverse (filter (\x -> x == SuperUp || x == SuperDown) a)) ++ [SuperDown]) !! 0 == SuperUp
@@ -47,7 +47,10 @@ test = [scoreFromString "M" == 1,
         scoreFromString "MSu" == 2,
         scoreFromString "MRu" == 4,
         scoreFromString "MRuRd" == 7,
-        scoreFromString "MTuRuCRd" == 10
+        scoreFromString "MTuRuCRd" == 12,
+        scoreFromString "MCCCCCC" == 13,
+        scoreFromString "MSuSdSuCCTuC" == 32,
+        scoreFromString "MTuCTuCTuC" == 15
        ]
 
 allPass :: [Bool] -> Bool
@@ -55,4 +58,4 @@ allPass = all (== True)
 
 main :: IO()
 main = do args <- getArgs;
-          if allPass test then (putStrLn . show . score . tokenize) (head args) else error "tests fail";
+          if allPass test then (putStrLn . show . score . tokenize) (head args) else error ("tests fail: " ++ (show test));
